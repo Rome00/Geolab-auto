fd95ddfb3c11d5217bc12dd35b0a8147d3a37e7c
Rome00 <romeo.khazalia@geolab.edu.ge>
2019-05-31 17:52:59 +0400

    finish

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector('.burger');
 const logo = document.querySelectorAll('.navbar-brand img');
 const navlinks = document.querySelector('.navbar-nav');
+const link = document.querySelector('.nav-link');
 var variable = true;
 burger.addEventListener('click', () => {
     burger.classList.toggle('Burgeractive');
     navlinks.classList.toggle('navbar-nav-active');
+    link.classList.toggle('nav-link-active');
     if (variable) {
         logo[1].classList.remove('hidden');
         variable = false;
@@ -13,3 +15,48 @@ burger.addEventListener('click', () => {
         variable = true;
     }
 })
+
+$(document).ready(function () {
+    $('form').submit(function (e) {
+        var name = 0;
+        var mail = 0;
+        var subject = 0;
+        var text = 0;
+        if ($("#name").val() === "") {
+            $('#span-name').css("opacity", 1);
+            name = 1;
+        } else {
+            $('#span-name').css("opacity", 0);
+            errormessige = 0;
+        }
+        if ($("#mail").val() === "") {
+            $('#span-mail').css("opacity", 1)
+            mail = 1;
+
+        } else {
+            $('#span-mail').css("opacity", 0);
+            mail = 0;
+        }
+        if ($("#subject").val() === "") {
+            $('#span-subject').css("opacity", 1)
+            suject = 1;
+
+        } else {
+            $('#span-subject').css("opacity", 0);
+            subject = 0;
+        }
+        if ($("#textarea").val() === "") {
+            $('#span-text').css("opacity", 1)
+            text = 1;
+
+        } else {
+            $('#span-text').css("opacity", 0);
+            text = 0;
+        }
+        if (name == 0 && mail == 0 && subject == 0 && text == 0) {
+            $('#success').css('opacity', 1);
+            return;
+        }
+        e.preventDefault();
+    });
+});
